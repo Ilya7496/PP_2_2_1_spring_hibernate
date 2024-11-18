f62747afd3399ac39924614138425235d3e31642
ilyamikhailov <79221803601me@gmail.com>
2024-11-18 18:24:32 +0300

    Изменения

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,24 +1,35 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 
 import java.sql.SQLException;
 import java.util.List;
 
 public class MainApp {
    public static void main(String[] args) throws SQLException {
-      AnnotationConfigApplicationContext context = 
+      AnnotationConfigApplicationContext context =
             new AnnotationConfigApplicationContext(AppConfig.class);
 
       UserService userService = context.getBean(UserService.class);
+      User user1 = new User("Дима", "Иванов","Иванов@gmail.com");
+      User user2 = new User("Вова", "Петров","Петров@gmail.com");
+      User user3 = new User("Сережа", "Ильин","Ильин@gmail.com");
+      User user4 = new User("Ваня", "Сергеев","Сергеев@gmail.com");
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      Car car1 = new Car("Audi",1);
+      Car car2 = new Car("BMW",2);
+      Car car3 = new Car("Mercedez",3);
+      Car car4 = new Car("Porsche",4);
+
+      userService.add(user1.setCar(car1).setUser(user1));
+      userService.add(user2.setCar(car2).setUser(user2));
+      userService.add(user3.setCar(car3).setUser(user3));
+      userService.add(user4.setCar(car4).setUser(user4));
 
       List<User> users = userService.listUsers();
       for (User user : users) {
@@ -28,7 +39,6 @@ public class MainApp {
          System.out.println("Email = "+user.getEmail());
          System.out.println();
       }
-
       context.close();
    }
 }
